654f75ebabca946bf18c3865213a6ffd9d7d1626
andi faizal <andi.faizal@stockbit.com>
2020-03-04 06:27:45 +0700

    feat($preposttest): add method to count start & end time test

--- evaluation/model/assesment.go
+++ evaluation/model/assesment.go
@@ -1,10 +1,22 @@
 package model
 
+import (
+	"time"
+)
+
 type Assesment struct {
 	Start        string
 	End          string
 	Status       string
 	QuestionList []*Question
+}
+
+func (a *Assesment) SetDuration() {
+	layout := "2006-01-02 15:04:05"
+	startTime := time.Now()
+	endTime := startTime.Add(time.Hour * 2)
+	a.Start = startTime.Format(layout)
+	a.End = endTime.Format(layout)
 }
 
 func (a *Assesment) AddQuestion(Q *Question) {
